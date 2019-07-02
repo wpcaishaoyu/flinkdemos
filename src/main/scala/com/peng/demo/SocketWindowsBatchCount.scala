9c2698b732d1944f3cb4ac96a70f9ad3f763f9a4
wangpeng <wangpeng@igengmei.com>
2019-07-02 19:36:33 +0800

    添加batch word count

---
+++ src/main/scala/com/peng/demo/SocketWindowsBatchCount.scala
@@ -0,0 +1,20 @@
+package com.peng.demo
+
+import org.apache.flink.api.scala.ExecutionEnvironment
+import org.apache.flink.streaming.api.scala._
+
+object SocketWindowsBatchCount {
+  def main(args: Array[String]): Unit = {
+    val env: ExecutionEnvironment = ExecutionEnvironment.getExecutionEnvironment
+    //获取数据
+    print(ClassLoader.getSystemResource(""))
+    val outPut = ClassLoader.getSystemResource("") + "output.txt"
+    val text = env.readTextFile(ClassLoader.getSystemResource("") + "word.txt")
+    val counts = text.flatMap(_.toLowerCase.split("\\W+"))
+      .filter(_.nonEmpty)
+      .map((_, 1)).groupBy(0)
+      .sum(1)
+    counts.writeAsCsv(outPut, "\n", " ").setParallelism(1)
+    env.execute("batch word count")
+  }
+}
