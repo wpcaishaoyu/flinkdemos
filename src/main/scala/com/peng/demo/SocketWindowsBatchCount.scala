0f26a339cfad91a9b0d70fdb5ebffc2923b8b15a
wangpeng <wangpeng@igengmei.com>
2019-07-02 19:44:06 +0800

    添加注释

--- src/main/scala/com/peng/demo/SocketWindowsBatchCount.scala
+++ src/main/scala/com/peng/demo/SocketWindowsBatchCount.scala
@@ -1,15 +1,18 @@
 package com.peng.demo
 
+import org.apache.flink.api.scala.DataSet
 import org.apache.flink.api.scala.ExecutionEnvironment
 import org.apache.flink.streaming.api.scala._
 
 object SocketWindowsBatchCount {
   def main(args: Array[String]): Unit = {
+    //批处理
     val env: ExecutionEnvironment = ExecutionEnvironment.getExecutionEnvironment
     //获取数据
     print(ClassLoader.getSystemResource(""))
     val outPut = ClassLoader.getSystemResource("") + "output.txt"
-    val text = env.readTextFile(ClassLoader.getSystemResource("") + "word.txt")
+    //source的返回值不同
+    val text: DataSet[String] = env.readTextFile(ClassLoader.getSystemResource("") + "word.txt")
     //各中算子的操作和返回值
     val counts = text.flatMap(_.toLowerCase.split("\\W+"))
       .filter(_.nonEmpty)
